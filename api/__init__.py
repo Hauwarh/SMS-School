eeebc1e043b5ba9020679a903c909bab14848cb7
Hauwa Ibrahim <umeey25@gmail.com>
2023-03-11 23:46:19 +0100

    Added models

--- api/__init__.py
+++ api/__init__.py
@@ -1,2 +1,9 @@
 from flask import Flask
 from flask_restx import Api
+from .course .views import course_namespace
+from .auth.views import auth_namespace
+from .config.config import config_dict
+from .utils import db
+from .models.course import Order
+from .models.users import User
+
---
+++ api/auth/views.py
@@ -0,0 +1,4 @@
+from flask import request
+from flask_restx import Namespace, Resource, fields
+
+auth_namespace = Namespace('auth', description='Namespace for Authentication')
